089ddc8c6765477ca7a0eeb42b25e31a19450e7b
gavin <gavin@fsn-gavin1>
2020-12-07 15:10:52 +0200

    undo all changes

--- drivers/staging/rtl8723cs-new/os_dep/linux/ioctl_cfg80211.c
+++ drivers/staging/rtl8723cs-new/os_dep/linux/ioctl_cfg80211.c
@@ -6928,13 +6928,12 @@ static struct cfg80211_ops rtw_cfg80211_ops = {
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 37)) || defined(COMPAT_KERNEL_RELEASE)
 	.mgmt_tx = cfg80211_rtw_mgmt_tx,
-#endif
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0))
 	.update_mgmt_frame_registrations = cfg80211_rtw_update_mgmt_frame_registrations,
 #else
 	.mgmt_frame_register = cfg80211_rtw_mgmt_frame_register,
 #endif
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 34) && LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 35))
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 34) && LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 35))
 	.action = cfg80211_rtw_mgmt_tx,
 #endif
 
